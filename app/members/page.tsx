72a2a093ec6ef36ea2359317de4d5854219f17e9
Simon Burmer <simon.burmer@sap.com>
2025-11-23 01:08:41 +0100

    add dym rendering 2

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -5,6 +5,8 @@ import Link from "next/link"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import Script from "next/script"
 
+export const dynamic = 'force-dynamic'
+
 interface Member {
   id: number
   name: string
